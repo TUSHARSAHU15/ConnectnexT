5e91456341319f4153d318ff45911e264858cd0d
TUSHARSAHU15 <tusharsahu1511@gmail.com>
2026-05-31 18:57:04 +0530

    fix: dynamically allow requesting CORS origin

--- backend/server.js
+++ backend/server.js
@@ -28,7 +28,7 @@ const app = express();
 // Security Middlewares
 app.use(helmet());
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, 'http://localhost:5173', 'http://127.0.0.1:5173'].filter(Boolean),
+  origin: true,
   credentials: true
 }));
 
